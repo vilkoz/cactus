a807a5011e1bc0c1c02fe377e3da0306cc3cca94
Kirsian <dvzabolotny@gmail.com>
2017-05-13 20:51:01 +0300

    push frontend 0.3

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/index.css">
+	<title>Leisure Club</title>
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+	  <div class="container-fluid">
+
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">
+			  <img src="logo.png" class="logo">
+			  <!-- <span class="brand-name">Leisure Club</span> -->
+		  </a>
+	    </div>
+
+
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="#">Profile<span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">Find Event</a></li>
+			<li><a href="#">About us</a></li>
+	        <!-- <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	          <ul class="dropdown-menu" role="menu">
+	            <li><a href="#">Action</a></li>
+	            <li><a href="#">Another action</a></li>
+	            <li><a href="#">Something else here</a></li>
+	            <li class="divider"></li>
+	            <li><a href="#">Separated link</a></li>
+	            <li class="divider"></li>
+	            <li><a href="#">One more separated link</a></li>
+	          </ul>
+	        </li> -->
+	      </ul>
+	      <form id="signin" class="navbar-form navbar-right" role="form">
+	                        <div class="input-group">
+	                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+	                            <input id="email" type="email" class="form-control" name="email" value="" placeholder="Email Address">
+	                        </div>
+
+	                        <div class="input-group">
+	                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+	                            <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">
+	                        </div>
+
+	                        <button type="submit" class="btn btn-primary">Login</button>
+	       </form>
+	    </div>
+	  </div>
+	</nav>
+	<div class="wrap">
+		<div class="row panels">
+			<div class="col-md-1">
+			</div>
+			<div class="col-md-5">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						1
+					</div>
+					<div class="panel-body">
+						qwe
+					</div>
+				</div>
+			</div>
+			<div class="col-md-5">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						2
+					</div>
+					<div class="panel-body">
+						asd
+					</div>
+				</div>
+			</div>
+			<div class="col-md-1">
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="container">
+			<div class="row">
+			</div>
+		</div>
+		<div class="footer-bottom">
+			<div class="row">
+				
+			</div>
+			<p class="pull-left">LC Leisure Club all rights reserved</p>
+		</div>
+	</div>
+</body>
+<html>
